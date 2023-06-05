2c64b073669a18a449cdf105f8aace0127de29c2
16ise <16isen.rukiye@gmail.com>
2023-06-05 20:43:29 +0300

    sonddegişim

--- finalProject/calisanİslemler.py
+++ finalProject/calisanİslemler.py
@@ -37,11 +37,20 @@ def calisan_response():
 
 def hesapla_performance(sent):
     text = sent.split(",")
-    result = employee_analysis(int(text[0]),int(text[1]),text[2],text[3],int(text[4]),int(text[5]),int(text[6]))[0]
+    result = employee_analysis(int(text[0]), int(text[1]), text[2], text[3], int(text[4]), int(text[5]), int(text[6]))[
+        0]
     if result == "Yes":
-        return "Tahmin: Evet"
+        return "Yapılan tahmine göre yaşı: " + text[0] + ", günlük oran değeri: " + text[1] + ", departmanı: " + text[2] \
+               + ", eğitim alanı: " + text[3] + ", şirkette " + text[4] + " yıldır çalışan, " + "şu anki pozisyonunda " \
+               + text[5] + " yıldır çalışan ve " + "en son promotiondan " + text[
+                   6] + " yıl geçen çalışanın yıpranma oranı yüksektir"
+
     elif result == "No":
-        return "Tahmin: Hayır"
+        return "Yapılan tahmine göre yaşı: " + text[0] + ", günlük oran değeri: " + text[1] + ", departmanı: " + text[2] \
+               + ", eğitim alanı: " + text[3] + ", şirkette " + text[4] + " yıldır çalışan, " + "şu anki pozisyonunda " \
+               + text[5] + "yıldır çalışan ve " + "en son promotiondan " + text[
+                   6] + " yıl geçen çalışanın yıpranma oranı düşüktür"
+
 
 #print(employee_analysis(32,279,'Sales','Life Sciences',6,4,0))
 #print(hesapla_performance("43, 1273, Research & Development, Medical, 3, 4, 5"))
